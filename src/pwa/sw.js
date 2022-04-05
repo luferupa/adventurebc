99c8f91961699d2fd7c030fcbad7f8a55fddce1c
luferupa <luferupa@gmail.com>
2022-04-05 11:14:27 -0700

    Added offline page v1

--- src/pwa/sw.js
+++ src/pwa/sw.js
@@ -1,25 +1,48 @@
 'use strict';
 
-const cacheName = 'pwa-cache';
-const contentToCache = ['./icons/icon-192x192.png'];
+const cacheName = 'pwa-offline';
+const contentToCache = ['../pages/guide/guide.html', '../pages/guide/guide.scss'];
+const offlineUrl = "./offline.html";
 
 self.addEventListener('fetch', function( event ) {
-    //console.log(`Fetching ${event.request.url}`);    
-      event.respondWith( 
-        fetch( event.request )
+    console.log(`Fetching ${event.request.url}`);  
+    if(event.request.mode == "navigate"){  
+      console.log("NAVIGATION");
+      /*event.respondWith( 
+        caches.match(event.request).then(function(response){
+          return response || fetch(event.request)
+        })
+
+      );*/
+      event.respondWith(
+        (async() => {
+           return fetch(event.request)
+              .catch(async (error) =>{
+                console.log("ERROR");
+                let cache = await caches.open(cacheName);
+                console.log(cache);
+                let response = await cache.match(offlineUrl);
+                return response;
+              })
+        })()
+        
+      );
+              
+    }
+});
+
+/*fetch( event.request )
         .catch(err => {
           console.log(1235);
           return caches.match(event.request);
-        })
-      );
-});
+        })*/
 
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Install');
-  event.waitUntil(async () => {
-    let cache = await caches.open(cacheName);
-    return await cache.addAll(contentToCache);
-  })
-  console.log("Installed");
+  event.waitUntil(
+     (async () => {
+        let cache = await caches.open(cacheName);
+        await cache.add(new Request(offlineUrl, { cache: "reload"}));
+      })()
+  );
   self.skipWaiting();
 });
